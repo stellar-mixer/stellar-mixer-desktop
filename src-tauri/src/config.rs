a7a1774841808ef0af0af0f1c6253fb4474d3dce
coolman <coolman@coolmans-MacBook-Pro.local>
2026-07-03 01:20:02 +0300

    good run

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -1,24 +1,40 @@
 pub const APP_SERVICE: &str = "stellar-mixer-desktop";
 pub const VAULT_KEY: &str = "main-vault";
 
-pub const TREEPIR_URL: &str = "http://127.0.0.1:3000";
-pub const MIXER_ARCHIVE_URL: &str = "http://127.0.0.1:3001";
+pub const TREEPIR_URL: &str = "http://213.171.26.211:3000";
+pub const MIXER_ARCHIVE_URL: &str = "http://213.171.26.211:3001";
 pub const GROTH16_WRAP_URL: &str = "http://213.171.26.211:8080/wrap";
 
 pub const STELLAR_RPC_URL: &str = "https://soroban-rpc.testnet.stellar.gateway.fm";
-pub const MIXER_CONTRACT_ID: &str = "CCVPF5JH57FQV535OYMWWY3VXUC53JEMCUPQIBM6NQKI3FZ5BZ47WVRL";
+pub const MIXER_CONTRACT_ID: &str = "CCO2BNPJQENNYXHYE5JWGNX74SNHZT74V3OL2IFCJOVZHEJVO2KVWEN5";
 
-pub const DEPTH: usize = 35;
+pub const DEPTH: usize = 45;
 
-pub const TRANSFER_GUEST_ELF_PATH: &str = "/Users/coolman/Code/stellar-zk-mixer-risc0-prover/target/riscv-guest/methods/transfer_guest/riscv32im-risc0-zkvm-elf/release/transfer_guest.bin";
-pub const WITHDRAW_GUEST_ELF_PATH: &str = "/Users/coolman/Code/stellar-zk-mixer-risc0-prover/target/riscv-guest/methods/withdraw_guest/riscv32im-risc0-zkvm-elf/release/withdraw_guest.bin";
+pub const TRANSFER_GUEST_ELF_PATH: &str =
+    concat!(env!("CARGO_MANIFEST_DIR"), "/guests/transfer_guest.bin");
+pub const WITHDRAW_GUEST_ELF_PATH: &str =
+    concat!(env!("CARGO_MANIFEST_DIR"), "/guests/withdraw_guest.bin");
 
 pub const TRANSFER_GUEST_ID: [u32; 8] = [
-    1806926303, 881112581, 2328359865, 3507547059, 3107974702, 3612662461, 100106614, 1141583706,
+    498159648,
+    2938926520,
+    1961483861,
+    318186971,
+    124339936,
+    981829501,
+    2383877649,
+    2412115495,
 ];
 
 pub const WITHDRAW_GUEST_ID: [u32; 8] = [
-    2068924445, 4135432564, 1502870765, 2851623954, 19087385, 1290228615, 1923192755, 2248842611,
+    58232140,
+    1509054718,
+    113098096,
+    1232102061,
+    150807468,
+    3067204521,
+    460803923,
+    1733823451,
 ];
 
 pub const DEFAULT_DEPOSIT_AMOUNT: u128 = 10_000_000;
